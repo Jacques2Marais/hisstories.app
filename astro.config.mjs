1e92f9a2d42ad5c41f3cd37633f0c90ffa66b8ad
Jacques <24863947@sun.ac.za>
2024-04-11 13:35:30 +0200

    Init site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [
+    svelte(), 
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ],
+  output: "server",
+  adapter: vercel()
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,38 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'hs-blue': {
+					'95': '#1e1d2f',
+					'90': '#2B2A4C',
+					'75': '#403f64',
+					'60': '#545379',
+					'45': '#6b6991',
+					'30': '#8280ab',
+				},
+				'hs-red': {
+					'90': '#89221a',
+					'80': '#9f271e',
+					'70': '#a82d1f',
+					'60': '#b53e2c',
+					'50': '#c1543e',
+					'40': '#c8745f',
+					'30': '#db9985',
+					'20': '#e1aa99',
+				},
+				'hs-cream': {
+					'10': '#f4edeb',
+					'70': '#615756',
+					'90': '#3f3231'
+				},
+				'white': '#ffffff',
+			},
+		},
+		fontFamily: {
+			sans: 'Roboto, sans-serif',
+			serif: '"Lora Variable", serif'
+		},
 	},
 	plugins: [],
 }
